2e538800eb763ddcb16bb5e78778612df5c899e3
Qianmian Yu <im.qianmian.yu@gmail.com>
2021-11-16 18:51:08 +0800

    add nvim

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
 all: install
 
-install:
+UNAME_S := $(shell uname -s)
+
+install: alacritty nvim
+
+alacritty:
 	mkdir -p ~/.config/alacritty
 	[ -f ~/.config/alacritty/alacritty.yml ] || ln -s $(PWD)/alacritty.yml ~/.config/alacritty/alacritty.yml
 
-.PHONY: all install
+nvim:
+	[ -f ~/.config/nvim/ ] || ln -s $(PWD)/nvim ~/.config/nvim
+
+.PHONY: all install alacritty nvim
---
+++ nvim/init.lua
@@ -0,0 +1,272 @@
+-- Boostrapping packer
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+-- Define utils functions
+local utils = {}
+
+function utils.noremap(type, key, value, opts)
+  local options = {noremap = true}
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(type,key,value, options)
+end
+
+function utils.nnoremap(key, value, opts)
+  utils.noremap('n', key, value, opts)
+end
+
+-- ====Real Configuration====
+vim.g.mapleader = " "
+vim.wo.number = true
+-- Search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+-- Tabs
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+-- Left Key & Right Key
+utils.nnoremap('<left>', '^')
+utils.nnoremap('<right>', '$')
+-- Switch Tab
+utils.nnoremap('<leader>i', 'gT<CR>')
+utils.nnoremap('<leader>o', 'gt<CR>')
+-- Clipboard
+utils.noremap('v', '<leader>y', '"*y')
+utils.noremap('v', '<leader>p', '"*p')
+-- Split in reasonable positions
+vim.o.splitright = true
+vim.o.splitbelow = true
+-- Update quicker (mainly for git-gutter)
+vim.g.updatetime = 500
+
+utils.nnoremap('<leader>j', '<c-^>')
+
+-- Plugins
+vim.cmd [[packadd packer.nvim]]
+require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'hrsh7th/cmp-buffer' -- cmp buffer source
+
+  -- Syntax Highlight
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- Core Enhancement
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-surround'
+  use {'wakatime/vim-wakatime', event = 'VimEnter'}
+  use {'dstein64/vim-startuptime',  opt=true}
+
+  -- Fuzzy Finder
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- File Explorer
+   use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    -- config = function() require'nvim-tree'.setup {} end
+  }
+
+  -- Git
+  use 'tpope/vim-fugitive'
+  use 'airblade/vim-gitgutter'
+
+  -- UI Enhancement & Themes
+  use 'itchyny/lightline.vim'
+  use 'jacoborus/tender.vim'
+  use 'NLKNguyen/papercolor-theme'
+  use 'cormacrelf/dark-notify'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+-- ===== colorsheme settings =====
+vim.o.termguicolors = true
+-- vim.cmd("colorscheme tender")
+vim.g.lightline = { colorscheme = 'tender' }
+
+require('dark_notify').run({
+    schemes = {
+      -- you can use a different colorscheme for each
+      dark  = {
+	    colorscheme = "tender",
+		-- lightline=  "tender",
+	  },
+      light = {
+        colorscheme = "PaperColor",
+      }
+    }
+})
+
+-- ====LSP Settings====
+-- Completion
+-- https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
+local nvim_lsp = require('lspconfig')
+
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap('n', '<leader>cf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+-- local servers = { 'pyright', 'gopls', 'clangd', 'pylsp' }
+local servers = { 'gopls', 'clangd', 'pylsp' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    autostart = false,
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+nvim_lsp.rust_analyzer.setup {
+  autostart = false,
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  -- completion = {
+  --   autocomplete = false
+  -- },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+  },
+}
+
+-- Telescope Settings
+utils.nnoremap('<leader>b', '<cmd>Telescope buffers<cr>')
+utils.nnoremap('<leader>f', '<cmd>Telescope find_files<cr>')
+utils.nnoremap('<leader>h', '<cmd>Telescope oldfiles<cr>')
+utils.nnoremap('<leader>cc', '<cmd>Telescope commands<cr>')
+utils.nnoremap('<leader>ch', '<cmd>Telescope command_history<cr>')
+utils.nnoremap('<leader>l', '<cmd>Telescope live_grep<cr>')
+utils.nnoremap('<leader>z', '<cmd>Telescope spell_suggest<cr>')
+utils.nnoremap('<leader>pp', '<cmd>Telescope grep_string theme=dropdown<cr>')
+utils.noremap('','<F1>', '<cmd>Telescope help_tags<cr>')
+
+require('telescope').setup {
+  defaults = {
+    layout_strategy = "flex",
+    layout_config = {
+      flex = {
+        flip_columns = 150,
+      },
+    },
+  },
+}
+
+-- TreeSitter Settings
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {"cpp", "bash", "python", "go", "lua", "toml"},
+  highlight = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+}
+
+-- nvim-tree.lua
+require 'nvim-tree'.setup {}
+utils.nnoremap('<C-n>', ':NvimTreeToggle<CR>')
