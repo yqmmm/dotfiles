0a4258d67947bebb0a078289bef1356ddde38cd4
Qianmian Yu <im.qianmian.yu@gmail.com>
2022-04-14 00:03:06 +0800

    [nvim & tmux] Add vim-tmux-navigator.

--- nvim/init.lua
+++ nvim/init.lua
@@ -170,6 +170,7 @@ require('packer').startup(function()
 
   -- Misc
   use 'nathangrigg/vim-beancount'
+  use 'christoomey/vim-tmux-navigator'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
